f545e8633bccd8e5d5e4f53a828a789523353d6c
TakaraHideto <t-hideto-2411@occ.co.jp>
2024-05-07 17:48:01 +0900

    位置を変えました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-screen">
-      <div className="bg-blue-300 text-green-500 p-2 flex items-center justify-center">
+      <div className="bg-blue-300 text-green-500 p-2 flex items-center justify-start">
         訪問介護時間管理
       </div>
     </div>
