1b254892fcfed4ec997ac61b0f100ecbb99ada5d
MinKhantAungThwin <thwin-2413@occ.co.jp>
2024-05-09 15:35:03 +0900

    insert Helper and customer

--- prisma/migrations/20240509021531_/migration.sql
+++ prisma/migrations/20240509021531_/migration.sql
@@ -34,3 +34,11 @@ CREATE UNIQUE INDEX "Helper_helperID_key" ON "Helper"("helperID");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Customer_customerID_key" ON "Customer"("customerID");
+
+
+insert into Helper (id,helperID,helperName) values (1,1,"h1"),
+(2,2,"h2"),(3,3,"h3");
+
+insert into Customer(id,customerID,customerName) values (1,1,"c1"),(2,2,"c2"),(3,3,"c3");
+
+-- insert into AttendanceRecord(id,helperID,helperName,customerID,customerName,checkIn,checkOut)
