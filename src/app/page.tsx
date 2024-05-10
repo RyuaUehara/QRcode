5ff932f4b3ba9915f21207196e00ae456ff4359f
RyuaUehara <u-ryua-2410@occ.co.jp>
2024-05-10 09:58:57 +0900

    qrcode読み取り画面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CameraJsQR2 from "@/app/componets/CameraJsQR2";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
     <div className="w-screen">
       <div className="bg-black text-white p-2 flex items-center justify-center">
         訪問介護時間管理
+      </div>
+      <div className="flex h-screen items-center justify-center">
+        <CameraJsQR2 />
       </div>
     </div>
   );
